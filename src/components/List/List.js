b918939c56558f5237a713597e4bc73ad3a9a42d
Mariusz Białecki <mariusz.bialecki@onet.pl>
2019-10-28 18:23:40 +0100

    add Column.js and its connection to List.js

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import styles from './List.scss';
-import Hero from '../Hero/Hero'
+import Hero from '../Hero/Hero.js';
+import Column from '../Column/Column.js';
+import PropTypes from 'prop-types';
 
 class List extends React.Component {
+
+  static propTypes = {
+    title: PropTypes.node,
+    children: PropTypes.node,      
+  }
+
+  static defaultProps = {
+    children: <p>I can do all the things!!!</p>,
+  }
+
   render() {
     return (
-      <Hero />
-    )
+      <section className={styles.component}>        
+        <Hero titleText={this.props.title} 
+        heroImage={this.props.image} /> 
+        <div className={styles.description}>
+          {this.props.children}
+        </div>  
+        <div className={styles.columns}>
+          <Column title={'Animals'} />
+          <Column title={'Plants'} />
+          <Column title={'Minerals'} />
+        </div>     
+      </section>
+    );
   }
 }
 
