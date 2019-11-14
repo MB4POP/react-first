dedbc10c7bc95dfd44ee9c0af55761c252fe3f00
Mariusz Białecki <mariusz.bialecki@onet.pl>
2019-11-14 19:41:56 +0100

    add search results

---
+++ src/components/SearchResults/SearchResultsContainer.js
@@ -0,0 +1,17 @@
+/* eslint-disable linebreak-style */
+import {connect} from 'react-redux';
+import SearchResults from './SearchResults';
+import {createAction_changeSearchString} from '../../redux/searchStringRedux';
+import {getCardsForSearch} from '../../redux/CardsRedux';
+
+const mapStateToProps = (state, props) => ({
+  cards: getCardsForSearch(state, props.match.params.searchString),
+  columns: state.columns,
+  lists: state.lists,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  changeSearchString: newSearchString => dispatch(createAction_changeSearchString(newSearchString)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(SearchResults);
--- src/redux/cardsRedux.js
+++ src/redux/cardsRedux.js
@@ -2,23 +2,24 @@
 import shortid from 'shortid';
 
 // selectors
-export const getCardsForColumn = ({cards, searchString}, columnId) => cards.filter(card => (card.columnId == columnId) 
-&& (new RegExp(searchString, 'i').test(card.title)));
-
+export const getCardsForColumn = ({cards}, columnId) => cards.filter(card => (card.columnId == columnId));
+export const getCardsForSearch = ({cards}, searchString) => cards.filter(card => new RegExp(searchString, 'i').test(card.title));
 // action name creator
 const reducerName = 'cards';
 const createActionName = name => `app/${reducerName}/${name}`;
 
 // action types
 export const ADD_CARD = createActionName('ADD_CARD');
-
+export const MOVE_CARD = createActionName('MOVE_CARD');
 // action creators
 export const createAction_addCard = payload => ({ payload, type: ADD_CARD });
-
+export const createAction_moveCard = payload => ({ payload, type: MOVE_CARD });
 // reducer
 export default function reducer(state = [], action = {}) {
   switch (action.type) {
     case ADD_CARD:
+      return [...state, { ...action.payload, id: shortid.generate() }];
+    case MOVE_CARD: 
       return [...state, { ...action.payload, id: shortid.generate() }];
     default:
       return state;
